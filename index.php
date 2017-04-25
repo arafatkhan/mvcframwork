e47562ee82f9a7359ebd63572cdd3d11fcdf996b
ARAFAT KHAN <khanargon@gmail.com>
2017-04-25 14:11:31 +0600

    This is my first try

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include "inc/header.php"; ?>
+
+<?php 
+
+  include_once("system/lib/Main.php");
+  new Main();
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "inc/footer.php"; ?>
---
+++ system/lib/Main.php
@@ -0,0 +1,11 @@
+<?php 
+
+   class Main{
+       
+       
+       function __construct(){
+           echo "i am arafat from Main class";
+       }
+   }
+
+?>
